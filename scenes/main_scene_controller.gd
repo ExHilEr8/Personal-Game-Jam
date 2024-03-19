4d7f2c0c9cf1a682b65a7f1345fb384f4ab2b3d5
ExHilEr8 <justinkwietniowski@gmail.com>
2024-03-18 22:54:33 -0400

    Fixed naming nomenclature for MainSceneController.gd :)

--- scenes/main_scene_controller.gd
+++ scenes/main_scene_controller.gd
@@ -5,8 +5,3 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().change_scene_to_file(scene_to_set)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
